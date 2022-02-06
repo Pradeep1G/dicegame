c434cd1281a02d162b912936f4b3c3358db4bd14
Pradeep1G <98323512+Pradeep1G@users.noreply.github.com>
2022-02-06 17:09:00 +0530

    Update dice.js

--- dice.js
+++ dice.js
@@ -3,11 +3,11 @@ const x=["dice1.png", "dice2.png", "dice3.png", "dice4.png", "dice5.png", "dice6
 var y=Math.floor(Math.random()*6);
 var z=Math.floor(Math.random()*6);
 if(y>z){
-  document.querySelector("h1").textContent="Player 1 Wins!."
+  document.querySelector("h1").textContent="🚩Player 1 Wins!."
 }else if(z===y){
-  document.querySelector("h1").textContent="Both Are Tie!."
+  document.querySelector("h1").textContent="🎌Draw!.🎌"
 }else{
-  document.querySelector("h1").textContent="Player 2 Wins!."
+  document.querySelector("h1").textContent="Player 2 Wins!.🚩"
 }
 document.getElementById("img1").src=x[y];
 document.getElementById("img2").src=x[z];
